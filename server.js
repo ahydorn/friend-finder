098b5cb6c8b26e9ce56596ea36a723c4efe8e83c
ahydorn <adamhydorn@gmail.com>
2019-07-21 15:55:50 -0700

    boilerplate express setup in server.js, route requirements set

---
+++ server.js
@@ -0,0 +1,24 @@
+// Required packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Express setup
+const app = express();
+// Port setup
+const PORT = process.env.PORT || 3000;
+
+// bodyParser setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static('public'));
+
+require('./app/routing/apiRoutes');
+require('./app/routing/htmlRoutes');
+
+// Listener
+app.listen(PORT, function() {
+    console.log(`Listening on port ${PORT}`);
+});
