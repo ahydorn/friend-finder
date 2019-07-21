90d50fdfba03c777776502c9b1fcaa4b4b0aec78
ahydorn <adamhydorn@gmail.com>
2019-07-21 16:04:35 -0700

    added friends oobjects to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsList = [{
+        name: "Tom",
+        photo: "https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg",
+        preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+    },
+
+    {
+        name: "Tom Cat",
+        photo: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2Fa-%2FAAuE7mAP_QiyMYXKgxiahlcxjLROTzLgr4rUJ6hwjQ%3Ds900-mo-c-c0xffffffff-rj-k-no&f=1",
+        preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+    },
+
+    {
+        name: "Tom Brady",
+        photo: "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F575709b54bbe6f0387840167%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D81%26cropX2%3D670%26cropY1%3D64%26cropY2%3D654&f=1",
+        preferences: [1, 4, 3, 2, 4, 5, 4, 4, 4, 2]
+    },
+];
+
+module.exports = friendsList;
